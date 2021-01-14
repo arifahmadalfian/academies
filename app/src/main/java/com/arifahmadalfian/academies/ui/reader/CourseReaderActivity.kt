62ce0576fe56fc588f6fbfd74e58015d5b9d8414
arifahmadalfian <arifahmadalfian@gmail.com>
2021-01-14 20:06:52 +0700

    Menambahkan layout untuk ukuran peranti 600dp

--- app/src/main/java/com/arifahmadalfian/academies/ui/reader/CourseReaderActivity.kt
+++ app/src/main/java/com/arifahmadalfian/academies/ui/reader/CourseReaderActivity.kt
@@ -2,11 +2,17 @@ package com.arifahmadalfian.academies.ui.reader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.arifahmadalfian.academies.R
+import com.arifahmadalfian.academies.data.source.local.entity.ModuleEntity
 import com.arifahmadalfian.academies.ui.reader.content.ModuleContentFragment
 import com.arifahmadalfian.academies.ui.reader.list.ModuleListFragment
 import com.arifahmadalfian.academies.viewmodel.ViewModelFactory
+import com.arifahmadalfian.academies.vo.Resource
+import com.arifahmadalfian.academies.vo.Status
 
 class CourseReaderActivity : AppCompatActivity(), ICourseReaderCallback {
 
@@ -14,12 +20,20 @@ class CourseReaderActivity : AppCompatActivity(), ICourseReaderCallback {
         const val EXTRA_COURSE_ID = "extra_course_id"
     }
 
+    private var isLarge = false
+    private lateinit var viewModel: CourseReaderViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_reader)
 
+        if (findViewById<View>(R.id.frame_list) != null) {
+            isLarge = true
+        }
+
         val factory = ViewModelFactory.getInstance(this)
-        val viewModel = ViewModelProvider(this, factory)[CourseReaderViewModel::class.java]
+        viewModel = ViewModelProvider(this, factory)[CourseReaderViewModel::class.java]
+        viewModel.modules.observe(this, initObserver)
 
         val bundle = intent.extras
         if (bundle != null) {
@@ -33,20 +47,86 @@ class CourseReaderActivity : AppCompatActivity(), ICourseReaderCallback {
 
     private fun populateFragment() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        var fragment = supportFragmentManager.findFragmentByTag(ModuleListFragment.TAG)
-        if (fragment == null) {
-            fragment = ModuleListFragment.newInstance()
-            fragmentTransaction.add(R.id.frame_container, fragment, ModuleListFragment.TAG)
-            fragmentTransaction.addToBackStack(null)
+        if (!isLarge) {
+            var fragment = supportFragmentManager.findFragmentByTag(ModuleListFragment.TAG)
+            if (fragment == null) {
+                fragment = ModuleListFragment.newInstance()
+                fragmentTransaction.add(R.id.frame_container, fragment, ModuleListFragment.TAG)
+                fragmentTransaction.addToBackStack(null)
+            }
+            fragmentTransaction.commit()
+        } else {
+            var fragmentList = supportFragmentManager.findFragmentByTag(ModuleListFragment.TAG)
+            if (fragmentList == null) {
+                fragmentList = ModuleListFragment.newInstance()
+                fragmentTransaction.add(R.id.frame_list, fragmentList, ModuleListFragment.TAG)
+            }
+
+            var fragmentContent = supportFragmentManager.findFragmentByTag(ModuleContentFragment.TAG)
+            if (fragmentContent == null) {
+                fragmentContent = ModuleContentFragment.newInstance()
+                fragmentTransaction.add(R.id.frame_content, fragmentContent, ModuleContentFragment.TAG)
+            }
+            fragmentTransaction.commit()
+
         }
-        fragmentTransaction.commit()
+    }
+
+    private fun removeObserver() {
+        viewModel.modules.removeObserver(initObserver)
+    }
+
+    private fun getLastReadFromModule(moduleEntities: List<ModuleEntity>): String? {
+        var lastReadModule: String? = null
+        for (moduleEntitiy in moduleEntities) {
+            if (moduleEntitiy.read) {
+                lastReadModule = moduleEntitiy.moduleId
+                continue
+            }
+            break
+        }
+        return lastReadModule
+    }
+
+    private val initObserver: Observer<Resource<List<ModuleEntity>>> = Observer { modules ->
+        if (modules != null) {
+            when (modules.status) {
+                Status.LOADING -> {
+
+                }
+                Status.SUCCESS -> {
+                    val dataModules: List<ModuleEntity>? = modules.data
+                    if(dataModules != null && dataModules.isNotEmpty()) {
+                        val firstModule = dataModules[0]
+                        val isFirstModuleRead = firstModule.read
+
+                        if (!isFirstModuleRead) {
+                            val firstModuleId = firstModule.moduleId
+                            viewModel.setSelectedModule(firstModuleId)
+                        } else {
+                            val lastReadModuleId = getLastReadFromModule(dataModules)
+                            if (lastReadModuleId != null) {
+                                viewModel.setSelectedModule(lastReadModuleId)
+                            }
+                        }
+                    }
+                    removeObserver()
+                }
+                Status.ERROR -> {
+                    Toast.makeText(this, "Gagal menampilkan data.", Toast.LENGTH_SHORT).show()
+                    removeObserver()
+                }
+            }
+        }
     }
 
     override fun moveTo(position: Int, moduleId: String) {
-        val fragment = ModuleContentFragment.newInstance()
-        supportFragmentManager.beginTransaction().add(R.id.frame_container, fragment, ModuleContentFragment.TAG)
+        if (!isLarge) {
+            val fragment = ModuleContentFragment.newInstance()
+            supportFragmentManager.beginTransaction().add(R.id.frame_container, fragment, ModuleContentFragment.TAG)
                 .addToBackStack(null)
                 .commit()
+        }
     }
 
     override fun onBackPressed() {
